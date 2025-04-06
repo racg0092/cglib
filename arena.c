7c7c8f282d3cb6d33379e895ff244306060569ec
Richard Chapman Gomez <chapman.gomez.richard@gmail.com>
2025-04-06 13:58:40 -0400

    Initial Commit
    Added arena struct and functionality
    
    Added string struct and functionality

---
+++ arena.c
@@ -0,0 +1,54 @@
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  uint8_t *base; // Start of the memory block
+  size_t size;   // Total size of the arena
+  size_t offset; // Current offset for allocation
+} Arena;
+
+// Initializes the memory arena
+void arena_init(Arena *arena, size_t size) {
+  // cast the void* return typq of malloc
+  // to uint8_t for bitwise operation (for pointer arithmatic)
+  arena->base = (uint8_t *)malloc(size);
+  if (!arena->base) {
+    fprintf(stderr, "memory allocation failed\n");
+    exit(1);
+  }
+  arena->size = size;
+  arena->offset = 0; // set the save location to position 0
+}
+
+// Initializes the given pointer to the nearest alignment boundary
+// NOTE: i need to understand this one better
+size_t align_forward(size_t offset, size_t alignment) {
+  size_t reminder = offset % alignment;
+  if (reminder == 0)
+    return offset;
+  return offset + (alignment - reminder);
+}
+
+void *arena_alloc(Arena *arena, size_t size, size_t alignment) {
+  size_t aligned_offset = align_forward(arena->offset, alignment);
+
+  if (aligned_offset + size > arena->size) {
+    return NULL; // out of memory
+  }
+
+  void *ptr = arena->base + aligned_offset;
+  arena->offset = aligned_offset + size;
+  return ptr;
+}
+
+void arena_reset(Arena *arena) { arena->offset = 0; }
+
+void arena_free(Arena *arena) {
+  free(arena->base);
+  arena->base = NULL;
+  arena->size = 0;
+  arena->offset = 0;
+}
---
+++ string.c
@@ -0,0 +1,67 @@
+#include "arena.c"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+  uint8_t len;
+  char *str;
+  uint32_t capacity;
+} Str;
+
+Str new_str(const char *str, Arena *arena) {
+  if (!arena)
+    abort();
+
+  if (!str) {
+    Str string = {.len = 0, .capacity = 24};
+    string.str = (char *)arena_alloc(arena, 24, sizeof(uint8_t));
+    if (!string.str)
+      abort();
+    return string;
+  }
+
+  uint8_t len = 0;
+  for (;;) {
+    if (str[len] == '\0')
+      break;
+    len++;
+  }
+
+  Str string = {.len = len};
+
+  string.str = (char *)arena_alloc(arena, len + 1, sizeof(uint8_t));
+  if (!string.str) {
+    abort();
+  }
+
+  memcpy(string.str, str, len + 1);
+
+  return string;
+}
+
+// Print string to screen
+void print_str(Str *string) {
+  for (int i = 0; i < string->len; i++) {
+    printf("%c", string->str[i]);
+  }
+  printf("\n");
+}
+
+// Tursn a [Str] into upper case
+// TODO: the difference between upper case and lower case is always 32
+void to_upper(Str *string) {}
+
+bool str_equals(Str string, const char *str) {
+  bool eq = true;
+  for (int i = 0; i < string.len; i++) {
+    if (string.str[i] != str[i]) {
+      eq = false;
+      break;
+    }
+  }
+  return eq;
+}
